710bd7594bb0435fd5f156a7cf82d2f0b1350c38
demir45 <demir45u@yahoo.com>
2022-02-07 23:18:32 +0300

    add admin

--- src/main/java/com/bank/BankofPrlMytryApplication.java
+++ src/main/java/com/bank/BankofPrlMytryApplication.java
@@ -61,11 +61,7 @@ class DemoCommandLineRunner implements CommandLineRunner{
 
 		// add admin
 		Optional<User> userAdmin = userRepo.findBySsn("123-45-6789");
-	/*	String encodedPassword = passwordEncoder.encode("123456");
-		userAdmin.get().setPassword(encodedPassword);
-		userRepo.save(userAdmin.get());
 
-	 */
 		if(!userAdmin.isPresent()){
 
 			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
